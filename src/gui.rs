121786f659f79b6dbbab84b5dec6d02243a4ea79
Phydon <leann.phydon@gmail.com>
2022-09-08 21:24:12 +0200

    polishing: color changes, removed side panels, fixed spaces; write to file; read file

--- src/gui.rs
+++ src/gui.rs
@@ -1,16 +1,18 @@
+// TODO read old notes from file on application start up
 use crate::lib::*;
 
 use eframe::egui;
 
 use std::collections::BTreeMap;
 
+const FILEPATH: &str = "./my_mininotes.txt";
 const WINDOW_HEIGHT: f32 = 600.0;
 const WINDOW_WIDTH: f32 = 960.0;
 const CENTER: (f32, f32) = (
     (WINDOW_WIDTH - WINDOW_WIDTH * 0.60),
     (WINDOW_HEIGHT - WINDOW_HEIGHT * 0.5),
 );
-const PADDING: f32 = 20.0;
+const PADDING: f32 = 10.0;
 
 #[derive(Default, PartialEq)]
 pub struct GuiMenu {
@@ -52,6 +54,25 @@ impl eframe::App for GuiMenu {
                 ui.with_layout(
                     egui::Layout::right_to_left(egui::Align::RIGHT),
                     |ui| {
+                        if ui.add(egui::Button::new("❌")).clicked() {
+                            self.on_close_event();
+                            egui::Window::new("Do you want to quit?")
+                                .collapsible(false)
+                                .resizable(false)
+                                .default_pos(CENTER)
+                                .show(ctx, |ui| {
+                                    ui.horizontal(|ui| {
+                                        if ui.button("Cancel").clicked() {
+                                            self.show_confirmation_dialog = false;
+                                        }
+
+                                        if ui.button("Yes!").clicked() {
+                                            self.allowed_to_close = true;
+                                            frame.close();
+                                        }
+                                    });
+                                });
+                        }
                         egui::reset_button(ui, self);
                     },
                 );
@@ -60,16 +81,16 @@ impl eframe::App for GuiMenu {
         });
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            ui.add_space(4.0);
+            ui.add_space(3.0);
             ui.vertical_centered(|ui| {
                 ui.label(
                     egui::RichText::new("MiniNotes")
                         .size(50.0)
                         .strong()
-                        .color(egui::Color32::from_rgb(171, 39, 79)),
+                        .color(egui::Color32::from_rgb(73, 166, 153)),
                 );
             });
-            ui.add_space(8.0);
+            ui.add_space(3.0);
         });
 
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
@@ -82,51 +103,21 @@ impl eframe::App for GuiMenu {
             ui.add_space(2.0);
         });
 
-        egui::SidePanel::left("left_panel").show(ctx, |ui| {
-            ui.add_space(2.0);
-            egui::containers::ScrollArea::vertical().show(ui, |ui| {
-                for i in 1..=60 {
-                    ui.label(format!("just some txt {}", i as usize));
-                }
-            });
-            ui.add_space(2.0);
-        });
-
-        egui::SidePanel::right("right_panel").show(ctx, |ui| {
-            ui.add_space(2.0);
-            egui::containers::ScrollArea::vertical().show(ui, |ui| {
-                ui.label("Right");
-            });
-            ui.add_space(2.0);
-        });
-
         // CentralPanel must be added after all other panels!
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add_space(2.0);
-            ui.heading("Take a Note");
-            ui.separator();
-
-            ui.horizontal(|ui| {
-                ui.label("Enter your note here: ");
-                ui.text_edit_singleline(&mut self.note_txt);
+            ui.vertical_centered(|ui| {
+                ui.label(egui::RichText::new("Take a Note: ")
+                    .heading()
+                    .strong()
+                    .color(egui::Color32::from_rgb(6, 165, 149)));
+                ui.text_edit_multiline(&mut self.note_txt);
             });
             ui.add_space(PADDING);
-            ui.horizontal(|ui| {
-                ui.colored_label(
-                    egui::Color32::from_rgb(128, 140, 255),
-                    "Your Note: ",
-                ); // Shortcut version
-                ui.label(
-                    egui::RichText::new(format!("{}", self.note_txt,))
-                        .size(20.0)
-                        .italics(),
-                );
-            });
 
             ui.vertical_centered(|ui| {
                 if ui
-                    .add_sized([240., 80.], egui::Button::new("Save"))
-                    .on_hover_text("save your note")
+                    .add_sized([120., 25.], egui::Button::new("Save"))
                     .clicked()
                 {
                     self.idx = get_date_and_time();
@@ -136,31 +127,36 @@ impl eframe::App for GuiMenu {
                         &self.note_txt,
                     ) {
                         Ok(()) => {
-                            println!("Note stored");
+                            match write_to_file(FILEPATH, &self.storage) {
+                                Ok(()) => (),
+                                Err(err) => println!("Unable to write to file: {err}"),
+                            }
                         }
                         Err(err) => println!("Unable to store note: {err}"),
                     }
                 }
-                ui.add_space(PADDING + 10.0);
+                ui.add_space(PADDING);
             });
-            ui.separator();
             ui.separator();
 
             ui.add_space(2.0);
-            egui::containers::ScrollArea::vertical().show(ui, |ui| {
-                for (key, value) in &self.storage {
-                    ui.horizontal(|ui| {
-                        ui.label(
-                            egui::RichText::new(format!("{}:: ", key,))
-                                .size(20.0)
-                                .color(egui::Color32::from_rgb(110, 255, 110)),
-                        );
-                        ui.label(
-                            egui::RichText::new(format!("{}", value,))
-                                .size(25.0),
-                        );
-                    });
-                }
+            ui.vertical_centered(|ui| {
+                egui::containers::ScrollArea::both().show(ui, |ui| {
+                    for (key, value) in &self.storage {
+                        ui.horizontal(|ui| {
+                            ui.label(
+                                egui::RichText::new(format!("{}:: ", key,))
+                                    .size(20.0)
+                                    .color(egui::Color32::from_rgb(76, 116, 166)),
+                            );
+                            ui.label(
+                                egui::RichText::new(format!("{}", value,))
+                                    .size(25.0)
+                                    .color(egui::Color32::from_rgb(22, 146, 196)),
+                            );
+                        });
+                    }
+                });
             });
             ui.add_space(2.0);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use chrono::Local;
 
-use std::{collections::BTreeMap, error::Error};
+use std::{collections::BTreeMap, error::Error, fs, io::{prelude::*, BufReader, Write}};
 
 pub fn get_date_and_time() -> String {
     Local::now().to_string().replace(":", "_")
@@ -12,5 +12,31 @@ pub fn store_notes(
     note: &str,
 ) -> Result<(), Box<dyn Error>> {
     storage.insert(idx.to_string(), note.to_string());
+    Ok(())
+}
+
+// FIXME return BTreeMap instead of Vec
+pub fn read_file(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let file = fs::OpenOptions::new().read(true).open(path)?;
+
+    let reader = BufReader::new(file);
+    let mut storage: Vec<String> = Vec::new();
+
+    for line in reader.lines() {
+        storage.push(line.unwrap());
+    }
+
+    Ok(storage)
+}
+
+pub fn write_to_file(path: &str, content: &BTreeMap<String, String>) -> Result<(), Box<dyn Error>> {
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(path)?;
+
+    for (key, value) in content {
+        writeln!(file, "{}: {}", key, value)?;
+    }
     Ok(())
 }
